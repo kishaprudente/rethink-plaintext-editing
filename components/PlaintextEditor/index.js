2afe32ab552471d06d2b346031cc6064029d6776
Kisha Prudente <kisha.prudente@gmail.com>
2020-10-04 02:11:21 -0400

    added plain text editor

--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './style.css';
 
+//CKEDITOR
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 function PlaintextEditor({ file, write }) {
   console.log(file, write);
-  return (
+
+  const [editorLoaded, setEditorLoaded] = useState(false);
+  const [content, setContent] = useState('');
+  const [fileName, setFileName] = useState('');
+
+  useEffect(() => {
+    file.text().then(data => {
+      setContent(data);
+      setEditorLoaded(true);
+      setFileName(file.name.replace('/', ''));
+    });
+  }, [file]);
+
+  return editorLoaded ? (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
+      <h4 className={css.fileName}>{fileName}</h4>
+      <CKEditor editor={ClassicEditor} data={content} />
     </div>
+  ) : (
+    <div> Loading Editor </div>
   );
 }
 
