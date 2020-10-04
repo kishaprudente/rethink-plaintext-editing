f36f7f0fb60fa80951a3ea37299898ce3ab542b7
Kisha Prudente <kisha.prudente@gmail.com>
2020-10-04 16:40:46 -0400

    removed withCss plugin

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const withCSS = require("@zeit/next-css");
-const pipe = require("lodash/fp/pipe");
+// const withCSS = require('@zeit/next-css');
+// const pipe = require('lodash/fp/pipe');
 
-module.exports = pipe(withCSS)({
-  cssModules: true,
+// removed withCSS plugin. does not support css imports from node_modules
+module.exports = {
   webpack: config => {
     // Load SVGs inline
     config.module.rules.push({
       test: /\.svg$/,
-      use: { loader: "svg-inline-loader", options: {} }
+      use: { loader: 'svg-inline-loader', options: {} }
     });
     return config;
   }
-});
+};
